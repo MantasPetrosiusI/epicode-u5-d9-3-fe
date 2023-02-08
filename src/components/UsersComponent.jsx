add2fa2ed6a128b2d3a5b794ecc3da8ef7f29869
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-02-08 18:06:51 +0100

    Adds all files

---
+++ src/components/UsersComponent.jsx
@@ -0,0 +1,61 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { Alert, ListGroup, Container, Row, Table } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+
+const UsersComponent = () => {
+  const [users, setUsers] = useState([])
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const url = "http://localhost:3001/users"
+
+    const fetchData = async () => {
+      try {
+        const { data } = await axios("http://localhost:3001/users", {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        })
+        setUsers(data)
+        setError("")
+      } catch (error) {
+        console.log(error)
+        navigate("/login")
+      }
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <Container>
+      <Row className="justify-content-center mt-3">
+        {error && <Alert variant="danger">{error}</Alert>}
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Row>
+    </Container>
+  )
+}
+
+export default UsersComponent
